f6993e2532c6f9514364c08fcd00081afc93230a
Thy Chamroeunpiseth <seththy.sorry123@gmail.com>
2025-06-24 14:34:03 +0700

    fix frontend home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 // app/(root)/page.tsx
-import HomeClient from "@/components/shared/HomeClient";
+// import HomeClient from "@/components/shared/HomeClient";
 import { Suspense } from "react";
+import Home from "./home/HomePage";
 
 export default function HomePage() {
   return (
     <Suspense fallback={<div>Loading homepage...</div>}>
-      <HomeClient />
+      <Home />
     </Suspense>
   );
 }
